bd8656a58b95a1ade9007b833bd49dff3a6fff00
Paul Thompson <pthompso@akamai.com>
2021-09-24 23:14:18 -0400

    PATCH, DELETE, POST, GET all functional for suppliers

--- greenknight/views.py
+++ greenknight/views.py
@@ -26,6 +26,7 @@ class ProductList(APIView):
 class SupplierList(viewsets.ModelViewSet):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializer
+
     def list(self, request):
         queryset = Supplier.objects.all()
         serializer = SupplierSerializer(queryset, many=True)
@@ -42,7 +43,7 @@ class SupplierList(viewsets.ModelViewSet):
         queryset = Supplier.objects.all()
         supplier = get_object_or_404(queryset, pk=pk)
         supplier.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response({"status": "deleted", "id": pk}, status=status.HTTP_204_NO_CONTENT)
 
     def create(self, request, pk=None):
 
@@ -50,8 +51,22 @@ class SupplierList(viewsets.ModelViewSet):
         if serializer.is_valid():
             self.perform_create(serializer)
 
-            return Response({serializer.data}, status=status.HTTP_201_CREATED)
+            return Response({"id": serializer.data['id'], "status": "created", "name": serializer.data['name']},
+                            status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+
+        queryset = Supplier.objects.all()
+        supplier = get_object_or_404(queryset, pk=pk)
+        serializer = SupplierSerializer(supplier, data=request.data, partial=True)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+
+            return Response({"id": serializer.data['id'], "name": serializer.data['name'],
+                             "status": "updated"}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class CategoryList(viewsets.ModelViewSet):
